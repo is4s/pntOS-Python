07bed3d440ccbcf241075fc9391f3a777982384d
wetrocks <henry.raquet@is4s.com>
2024-05-30 12:47:08 -0400

    Fixed colors

--- pntos-cobra/src/pntos/cobra/SimpleLoggingPluggin.py
+++ pntos-cobra/src/pntos/cobra/SimpleLoggingPluggin.py
@@ -17,6 +17,8 @@ class fmts:
     OKBLUE = "\033[94m"
     OKCYAN = "\033[96m"
     OKGREEN = "\033[92m"
+    DRKGRAY = "\033[90m"
+    LTGRAY = "\033[37m"
     WARNING = "\033[93m"
     FAIL = "\033[91m"
     ENDC = "\033[0m"
