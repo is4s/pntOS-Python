506dc43be247219f6aa5c92fd6609323ecd51781
Kyle Kauffman <code@kyonifer.com>
2024-06-13 15:14:16 -0400

    Fix removed symbols.

--- pntos-cobra/src/pntos/cobra/config/__init__.py
+++ pntos-cobra/src/pntos/cobra/config/__init__.py
@@ -1,8 +1,3 @@
-from .AlignmentConfig import (
-    ALIGNMENT_CONFIG_GYROCOMPASS as ALIGNMENT_CONFIG_GYROCOMPASS,
-)
 from .AlignmentConfig import AlignmentConfig as AlignmentConfig
-from .ImuConfig import IMU_CONFIG_COMMERCIAL as IMU_CONFIG_COMMERCIAL
-from .ImuConfig import IMU_CONFIG_TACTICAL as IMU_CONFIG_TACTICAL
 from .ImuConfig import ImuConfig as ImuConfig
 from .SensorConfig import SensorConfig as SensorConfig
