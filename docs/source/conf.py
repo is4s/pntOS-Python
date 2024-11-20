2f7e807d4715e07308ef04c21abbee3bcabec0ca
Daniel Carson <danielcarson271@gmail.com>
2024-11-20 12:52:31 -0600

    switch to using pntOS theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,5 +23,14 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
-html_static_path = ["_static"]
+html_theme = "sphinx_rtd_theme"
+branding_dir = "../branding_assets/branding/"
+html_static_path = ["../_static", branding_dir]
+
+html_logo = branding_dir + "pntOs_Logo_Gradient_Light_Horizontal.png"
+html_theme_options = {"logo_only": True}
+
+
+def setup(app):
+    app.add_css_file("pntos.css")
+    app.add_css_file("hk-grotesk.css")
