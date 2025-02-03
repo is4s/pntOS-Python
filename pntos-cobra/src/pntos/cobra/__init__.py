9c09fb61ed1e7abf925da5aab94b8d80768fe1d9
Henry Raquet <henry.raquet@is4s.com>
2025-02-03 17:27:03 +0000

    KeyValueStore API Update

--- pntos-cobra/src/pntos/cobra/__init__.py
+++ pntos-cobra/src/pntos/cobra/__init__.py
@@ -1,6 +1,15 @@
-from .SimpleControllerPlugin import SimpleControllerPlugin as SimpleControllerPlugin
+from .SimpleControllerPlugin import (
+    SimpleControllerPlugin as SimpleControllerPlugin,
+    SimpleMediator as SimpleMediator,
+)
+from .SimpleLoggingPlugin import SimpleLoggingPlugin as SimpleLoggingPlugin
 from .SimpleOrchestrationPlugin import (
     SimpleOrchestrationPlugin as SimpleOrchestrationPlugin,
 )
-from .SimpleRegistryPlugin import SimpleRegistryPlugin as SimpleRegistryPlugin
-from .SimpleTransportPlugin import SimpleTransportPlugin as SimpleTransportPlugin
+from .SimpleRegistryPlugin import (
+    SimpleRegistry as SimpleRegistry,
+    SimpleRegistryPlugin as SimpleRegistryPlugin,
+)
+from .SimpleTransportPlugin import (
+    SimpleTransportPlugin as SimpleTransportPlugin,
+)
