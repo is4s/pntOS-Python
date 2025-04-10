bbb6b891d922d403bc503fe55f05039a6d70985d
Daniel Carson <danielcarson271@gmail.com>
2025-04-10 12:54:22 -0400

    add postprocessing plotting tools
    increase resolution of saved figures
    
    remove unused imports
    
    don't lint postprocessing scripts

---
+++ postprocessing/plot_results.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from os import mkdir, path
+
+from lcm_stripper import harvest_by_channel
+from plots import plot_solution
+
+
+def plot_results(lcmlog):
+    pntos_solution_channel = '/solution/cobra/pva'
+    truth_channel = '/sensor/ins-d/pva'
+    channels_to_harvest = [truth_channel, pntos_solution_channel]
+
+    # Generate plots from LCM output
+    data = harvest_by_channel(lcmlog, channels_to_harvest)
+
+    log_dir = path.dirname(lcmlog)
+    filt = path.basename(lcmlog).split('.')[0]
+    filt_dir = path.join(log_dir, filt)
+    if not path.exists(filt_dir):
+        mkdir(filt_dir)
+
+    print(f'Plotting results...')
+    plot_solution(
+        [pntos_solution_channel, data[pntos_solution_channel]],  # solution
+        [truth_channel, data[truth_channel]],  # truth
+        filt_dir,
+        True,
+    )
+    print(f'Plots have been saved to {filt_dir}')
