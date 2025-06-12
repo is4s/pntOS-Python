434f79515dbadb966f757836f98f877b3119fd92
Daniel Carson <danielcarson271@gmail.com>
2025-06-12 15:09:39 -0400

    move static align to internal

--- pntos-cobra/src/pntos/cobra/internal.py
+++ pntos-cobra/src/pntos/cobra/internal.py
@@ -31,3 +31,4 @@ from .state_modeling_simple_gps_ins.PinsonVelocityMeasurementProcessor import (
 from .state_modeling_simple_gps_ins.PinsonWithNedFogmPositionMeasurementProcessor import (
     PinsonWithNedFogmPositionMeasurementProcessor as PinsonWithNedFogmPositionMeasurementProcessor,
 )
+from .StaticAlignInitializationPlugin import StaticAlign as StaticAlign
