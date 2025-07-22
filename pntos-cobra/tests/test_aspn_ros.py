f49ba4665797430cfa360f50dc7025ed004e169a
Charles Cochran <charles.cochran@is4s.com>
2025-07-22 13:51:32 -0400

    Add ASPN-ROS unit test
    When ROS is present, this test ensures that ASPN-ROS and the underlying
    ROS transport mechanism is working correctly.
    
    ROS is an optional dependency. So as not to fail for users or CI jobs
    that don't have ROS installed and sourced, this pytest is skipped when
    running in non-ROS environments.
    
    Before running the test, the job must activate the ASPN-ROS environment
    by cloning and sourcing /firehose-outputs/ros_devel/jazzy/setup.bash.
    Carson and I considered doing the clone during the build of the docker
    container, but we opted against it for two reasons:
    
    1. Since Docker layers using (or following those using) the CI token
       argument do not benefit from caching and the building of the Docker
       image happens in a before_script, the clone would actually happen
       separately for every single job instead of only the jobs using ROS.
    
    2. During local Docker builds (the CI uses tokens instead), secure
       clones using ssh authentication require the --ssh default flag and
       BuildKit. This works with docker and podman, but we are unable to get
       it to work using podman-desktop, limiting macOS support.
    
    Instead, we perform the clone in the container while it's running (only
    in the ROS-related jobs). To reproduce this locally, note that the
    container will have to be run with ssh key access. But this was already
    true because of the clones in the uv sync.
    
    Note that the sourcing of the setup file must happen in the same docker
    exec as the test itself since environments don't persist between docker
    exec calls.
    
    See #208

---
+++ pntos-cobra/tests/test_aspn_ros.py
@@ -0,0 +1,64 @@
+import os
+import time
+
+import numpy as np
+import pytest
+
+
+@pytest.mark.skipif(
+    'ROS_VERSION' not in os.environ, reason='Skip in non-ROS environments.'
+)
+def test_aspn_ros():
+    # ROS and ASPN-ROS imports should work here
+    import rclpy  # type: ignore[import]
+    from aspn23 import (  # type: ignore[import]
+        MeasurementPositionVelocityAttitude,
+        MeasurementPositionVelocityAttitudeErrorModel,
+        MeasurementPositionVelocityAttitudeReferenceFrame,
+        TypeHeader,
+        TypeTimestamp,
+    )
+    from aspn23_ros_utils import AspnMsg, AspnRosNode  # type: ignore[import]
+
+    rclpy.init()
+    node = AspnRosNode('test_aspn_ros')
+
+    expected = MeasurementPositionVelocityAttitude(
+        header=TypeHeader(vendor_id=1, device_id=1, context_id=1, sequence_id=0),
+        time_of_validity=TypeTimestamp(elapsed_nsec=time.time_ns()),
+        reference_frame=MeasurementPositionVelocityAttitudeReferenceFrame.ECI,
+        p1=1.0,
+        p2=2.0,
+        p3=3.0,
+        v1=4.0,
+        v2=5.0,
+        v3=6.0,
+        quaternion=np.zeros(4),
+        covariance=np.zeros(36).reshape(6, 6),
+        error_model=MeasurementPositionVelocityAttitudeErrorModel.NONE,
+        error_model_params=np.array([]),
+        integrity=[],
+    )
+    received = None
+
+    def aspn_cb(msg: AspnMsg):
+        nonlocal received
+        received = msg
+
+    # Publish and receive the message over ROS
+    topic = '/test_aspn_ros/pva'
+    node.subscribe_aspn(aspn_cb, topic, msg_type=type(expected))
+    node.publish_aspn(expected, topic)
+
+    # Wait for message to come to be received
+    rclpy.spin_once(node, timeout_sec=1.0)
+
+    # Ensure received message matches original message
+    #   Note: can't use "assert expected == received" because np.arrays have an
+    #   overridden equality operator
+    assert isinstance(received, MeasurementPositionVelocityAttitude)
+    for attr, value in expected.__dict__.items():
+        if isinstance(value, np.ndarray):
+            assert np.array_equal(value, getattr(received, attr))
+            continue
+        assert value == getattr(received, attr)
