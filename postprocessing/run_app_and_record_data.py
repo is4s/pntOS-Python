89d128cac2e29dadebce110a517b30201745f179
Braden Howell <braden.howell@is4s.com>
2025-09-11 18:13:05 +0000

    Be more verbose about plotting failure

--- postprocessing/run_app_and_record_data.py
+++ postprocessing/run_app_and_record_data.py
@@ -42,4 +42,6 @@ if __name__ == '__main__':
     if os.path.exists(OUTPUT_LOG_FILENAME):
         plot_results(OUTPUT_LOG_FILENAME, '/solution/pntos/pva', '/sensor/ins-d/pva')
     else:
-        print(f'pntOS failed to generate output log "{OUTPUT_LOG_FILENAME}"')
+        print(
+            f'Cannot plot results. pntOS failed to generate output log "{OUTPUT_LOG_FILENAME}"'
+        )
