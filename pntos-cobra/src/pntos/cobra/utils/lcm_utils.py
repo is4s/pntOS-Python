efd86fd8800fd5bb3bc23ac51edc07ccb70ccf4a
Eric McKanna <eric.mckanna@is4s.com>
2026-02-25 15:07:15 +0000

    Add mappings from firehose-outputs, specifically to expand support to Metadata

--- pntos-cobra/src/pntos/cobra/utils/lcm_utils.py
+++ pntos-cobra/src/pntos/cobra/utils/lcm_utils.py
@@ -6,55 +6,20 @@ from subprocess import PIPE, Popen
 
 import aspn2_translations
 import aspn23
-import aspn23_lcm
 import datasources.lcm.messages.aspn as aspn2_lcm
+from aspn23_lcm import (
+    AspnMsg as Aspn23Msg,
+    LcmMsg as Aspn23LcmMsg,
+    from_lcm_map as marshaler_from_aspn23_lcm,
+    to_lcm_map as marshaler_to_aspn23_lcm,
+)
 
 from pntos.api import LoggingLevel, Mediator, Message
 from pntos.cobra.config import AspnVersion
 
 from .apps import kill, monitor_app_output, run_app
 
-Aspn23LcmMeasurement = (
-    aspn23_lcm.measurement_angular_velocity_1d
-    | aspn23_lcm.measurement_angular_velocity
-    | aspn23_lcm.measurement_accumulated_distance_traveled
-    | aspn23_lcm.measurement_altitude
-    | aspn23_lcm.measurement_attitude_2d
-    | aspn23_lcm.measurement_attitude_3d
-    | aspn23_lcm.measurement_barometer
-    | aspn23_lcm.measurement_delta_position
-    | aspn23_lcm.measurement_delta_range
-    | aspn23_lcm.measurement_delta_range_to_point
-    | aspn23_lcm.measurement_direction_2d_to_points
-    | aspn23_lcm.measurement_direction_3d_to_points
-    | aspn23_lcm.measurement_direction_of_motion_2d
-    | aspn23_lcm.measurement_direction_of_motion_3d
-    | aspn23_lcm.measurement_frequency_difference
-    | aspn23_lcm.measurement_heading
-    | aspn23_lcm.measurement_image
-    | aspn23_lcm.measurement_IMU
-    | aspn23_lcm.measurement_magnetic_field
-    | aspn23_lcm.measurement_magnetic_field_magnitude
-    | aspn23_lcm.measurement_position
-    | aspn23_lcm.measurement_position_attitude
-    | aspn23_lcm.measurement_position_velocity_attitude
-    | aspn23_lcm.measurement_range_rate_to_point
-    | aspn23_lcm.measurement_range_to_point
-    | aspn23_lcm.measurement_satnav
-    | aspn23_lcm.measurement_satnav_subframe
-    | aspn23_lcm.measurement_satnav_with_sv_data
-    | aspn23_lcm.measurement_specific_force_1d
-    | aspn23_lcm.measurement_speed
-    | aspn23_lcm.measurement_TDOA_1Tx_2Rx
-    | aspn23_lcm.measurement_TDOA_2Tx_1Rx
-    | aspn23_lcm.measurement_temperature
-    | aspn23_lcm.measurement_time
-    | aspn23_lcm.measurement_time_difference
-    | aspn23_lcm.measurement_time_frequency_difference
-    | aspn23_lcm.measurement_velocity
-)
-
-Aspn2LcmMeasurement = (
+Aspn2LcmMsg = (
     aspn2_lcm.accumulateddistancetraveled
     | aspn2_lcm.altitude
     | aspn2_lcm.attitude1d
@@ -102,104 +67,18 @@ Aspn2LcmMeasurement = (
     | aspn2_lcm.velocity3d
 )
 
-Aspn23Measurement = (
-    aspn23.MeasurementAngularVelocity1D
-    | aspn23.MeasurementAngularVelocity
-    | aspn23.MeasurementAccumulatedDistanceTraveled
-    | aspn23.MeasurementAltitude
-    | aspn23.MeasurementAttitude2D
-    | aspn23.MeasurementAttitude3D
-    | aspn23.MeasurementBarometer
-    | aspn23.MeasurementDeltaPosition
-    | aspn23.MeasurementDeltaRange
-    | aspn23.MeasurementDeltaRangeToPoint
-    | aspn23.MeasurementDirection2DToPoints
-    | aspn23.MeasurementDirection3DToPoints
-    | aspn23.MeasurementDirectionOfMotion2D
-    | aspn23.MeasurementDirectionOfMotion3D
-    | aspn23.MeasurementFrequencyDifference
-    | aspn23.MeasurementHeading
-    | aspn23.MeasurementImage
-    | aspn23.MeasurementImu
-    | aspn23.MeasurementMagneticField
-    | aspn23.MeasurementMagneticFieldMagnitude
-    | aspn23.MeasurementPosition
-    | aspn23.MeasurementPositionAttitude
-    | aspn23.MeasurementPositionVelocityAttitude
-    | aspn23.MeasurementRangeRateToPoint
-    | aspn23.MeasurementRangeToPoint
-    | aspn23.MeasurementSatnav
-    | aspn23.MeasurementSatnavSubframe
-    | aspn23.MeasurementSatnavWithSvData
-    | aspn23.MeasurementSpecificForce1D
-    | aspn23.MeasurementSpeed
-    | aspn23.MeasurementTdoa1Tx2Rx
-    | aspn23.MeasurementTdoa2Tx1Rx
-    | aspn23.MeasurementTemperature
-    | aspn23.MeasurementTime
-    | aspn23.MeasurementTimeDifference
-    | aspn23.MeasurementTimeFrequencyDifference
-    | aspn23.MeasurementVelocity
-)
-
 # It is possible aspn2 lcm->aspn23 conversions to return non-Measurement types to get around
 # non-standard conversions, see for instance translate_from_aspn2_rangeratetoknownfeature_to_aspn23
 Aspn23MeasurementExtended = (
-    Aspn23Measurement
+    Aspn23Msg
     | list[aspn23.MeasurementDeltaRangeToPoint]
-    | aspn23.MetadataGpsLnavEphemeris
     | list[aspn23.MeasurementRangeRateToPoint]
     | list[aspn23.MeasurementRangeToPoint]
     | list[aspn23.MeasurementTdoa1Tx2Rx]
 )
 
-# dictionary mapping ASPN23 message type to ASPN23 LCM marshaling functions
-marshaler_to_aspn23_lcm: dict[
-    type[Aspn23Measurement], Callable[..., Aspn23LcmMeasurement]
-] = {
-    aspn23.MeasurementAngularVelocity1D: aspn23_lcm.measurement_angular_velocity_1d_to_lcm,
-    aspn23.MeasurementAngularVelocity: aspn23_lcm.measurement_angular_velocity_to_lcm,
-    aspn23.MeasurementAccumulatedDistanceTraveled: aspn23_lcm.measurement_accumulated_distance_traveled_to_lcm,
-    aspn23.MeasurementAltitude: aspn23_lcm.measurement_altitude_to_lcm,
-    aspn23.MeasurementAttitude2D: aspn23_lcm.measurement_attitude_2d_to_lcm,
-    aspn23.MeasurementAttitude3D: aspn23_lcm.measurement_attitude_3d_to_lcm,
-    aspn23.MeasurementBarometer: aspn23_lcm.measurement_barometer_to_lcm,
-    aspn23.MeasurementDeltaPosition: aspn23_lcm.measurement_delta_position_to_lcm,
-    aspn23.MeasurementDeltaRange: aspn23_lcm.measurement_delta_range_to_lcm,
-    aspn23.MeasurementDeltaRangeToPoint: aspn23_lcm.measurement_delta_range_to_point_to_lcm,
-    aspn23.MeasurementDirection2DToPoints: aspn23_lcm.measurement_direction_2d_to_points_to_lcm,
-    aspn23.MeasurementDirection3DToPoints: aspn23_lcm.measurement_direction_3d_to_points_to_lcm,
-    aspn23.MeasurementDirectionOfMotion2D: aspn23_lcm.measurement_direction_of_motion_2d_to_lcm,
-    aspn23.MeasurementDirectionOfMotion3D: aspn23_lcm.measurement_direction_of_motion_3d_to_lcm,
-    aspn23.MeasurementFrequencyDifference: aspn23_lcm.measurement_frequency_difference_to_lcm,
-    aspn23.MeasurementHeading: aspn23_lcm.measurement_heading_to_lcm,
-    aspn23.MeasurementImage: aspn23_lcm.measurement_image_to_lcm,
-    aspn23.MeasurementImu: aspn23_lcm.measurement_IMU_to_lcm,
-    aspn23.MeasurementMagneticField: aspn23_lcm.measurement_magnetic_field_to_lcm,
-    aspn23.MeasurementMagneticFieldMagnitude: aspn23_lcm.measurement_magnetic_field_magnitude_to_lcm,
-    aspn23.MeasurementPosition: aspn23_lcm.measurement_position_to_lcm,
-    aspn23.MeasurementPositionAttitude: aspn23_lcm.measurement_position_attitude_to_lcm,
-    aspn23.MeasurementPositionVelocityAttitude: aspn23_lcm.measurement_position_velocity_attitude_to_lcm,
-    aspn23.MeasurementRangeRateToPoint: aspn23_lcm.measurement_range_rate_to_point_to_lcm,
-    aspn23.MeasurementRangeToPoint: aspn23_lcm.measurement_range_to_point_to_lcm,
-    aspn23.MeasurementSatnav: aspn23_lcm.measurement_satnav_to_lcm,
-    aspn23.MeasurementSatnavSubframe: aspn23_lcm.measurement_satnav_subframe_to_lcm,
-    aspn23.MeasurementSatnavWithSvData: aspn23_lcm.measurement_satnav_with_sv_data_to_lcm,
-    aspn23.MeasurementSpecificForce1D: aspn23_lcm.measurement_specific_force_1d_to_lcm,
-    aspn23.MeasurementSpeed: aspn23_lcm.measurement_speed_to_lcm,
-    aspn23.MeasurementTdoa1Tx2Rx: aspn23_lcm.measurement_TDOA_1Tx_2Rx_to_lcm,
-    aspn23.MeasurementTdoa2Tx1Rx: aspn23_lcm.measurement_TDOA_2Tx_1Rx_to_lcm,
-    aspn23.MeasurementTemperature: aspn23_lcm.measurement_temperature_to_lcm,
-    aspn23.MeasurementTime: aspn23_lcm.measurement_time_to_lcm,
-    aspn23.MeasurementTimeDifference: aspn23_lcm.measurement_time_difference_to_lcm,
-    aspn23.MeasurementTimeFrequencyDifference: aspn23_lcm.measurement_time_frequency_difference_to_lcm,
-    aspn23.MeasurementVelocity: aspn23_lcm.measurement_velocity_to_lcm,
-}
-
 # dictionary mapping ASPN23 message type to ASPN2 LCM marshaling functions
-marshaler_to_aspn2_lcm: dict[
-    type[Aspn23Measurement], Callable[..., Aspn2LcmMeasurement]
-] = {
+marshaler_to_aspn2_lcm: dict[type[Aspn23Msg], Callable[..., Aspn2LcmMsg]] = {
     aspn23.MeasurementImu: aspn2_translations.translate_from_aspn23_imu_to_aspn2,
     aspn23.MeasurementPosition: aspn2_translations.translate_from_aspn23_position_to_aspn2_geodeticposition3d,
     aspn23.MeasurementPositionVelocityAttitude: aspn2_translations.translate_from_aspn23_positionvelocityattitude_to_aspn2,
@@ -207,47 +86,9 @@ marshaler_to_aspn2_lcm: dict[
 
 # dictionary mapping ASPN LCM message type to ASPN23 marshaling functions
 marshaler_from_lcm: dict[
-    type[Aspn23LcmMeasurement] | type[Aspn2LcmMeasurement],
+    type[Aspn2LcmMsg] | type[Aspn23LcmMsg],
     Callable[..., Aspn23MeasurementExtended],
 ] = {
-    # aspn23_lcm to aspn23
-    aspn23_lcm.measurement_angular_velocity_1d: aspn23_lcm.lcm_to_measurement_angular_velocity_1d,
-    aspn23_lcm.measurement_angular_velocity: aspn23_lcm.lcm_to_measurement_angular_velocity,
-    aspn23_lcm.measurement_accumulated_distance_traveled: aspn23_lcm.lcm_to_measurement_accumulated_distance_traveled,
-    aspn23_lcm.measurement_altitude: aspn23_lcm.lcm_to_measurement_altitude,
-    aspn23_lcm.measurement_attitude_2d: aspn23_lcm.lcm_to_measurement_attitude_2d,
-    aspn23_lcm.measurement_attitude_3d: aspn23_lcm.lcm_to_measurement_attitude_3d,
-    aspn23_lcm.measurement_barometer: aspn23_lcm.lcm_to_measurement_barometer,
-    aspn23_lcm.measurement_delta_position: aspn23_lcm.lcm_to_measurement_delta_position,
-    aspn23_lcm.measurement_delta_range: aspn23_lcm.lcm_to_measurement_delta_range,
-    aspn23_lcm.measurement_delta_range_to_point: aspn23_lcm.lcm_to_measurement_delta_range_to_point,
-    aspn23_lcm.measurement_direction_2d_to_points: aspn23_lcm.lcm_to_measurement_direction_2d_to_points,
-    aspn23_lcm.measurement_direction_3d_to_points: aspn23_lcm.lcm_to_measurement_direction_3d_to_points,
-    aspn23_lcm.measurement_direction_of_motion_2d: aspn23_lcm.lcm_to_measurement_direction_of_motion_2d,
-    aspn23_lcm.measurement_direction_of_motion_3d: aspn23_lcm.lcm_to_measurement_direction_of_motion_3d,
-    aspn23_lcm.measurement_frequency_difference: aspn23_lcm.lcm_to_measurement_frequency_difference,
-    aspn23_lcm.measurement_heading: aspn23_lcm.lcm_to_measurement_heading,
-    aspn23_lcm.measurement_image: aspn23_lcm.lcm_to_measurement_image,
-    aspn23_lcm.measurement_IMU: aspn23_lcm.lcm_to_measurement_IMU,
-    aspn23_lcm.measurement_magnetic_field: aspn23_lcm.lcm_to_measurement_magnetic_field,
-    aspn23_lcm.measurement_magnetic_field_magnitude: aspn23_lcm.lcm_to_measurement_magnetic_field_magnitude,
-    aspn23_lcm.measurement_position: aspn23_lcm.lcm_to_measurement_position,
-    aspn23_lcm.measurement_position_attitude: aspn23_lcm.lcm_to_measurement_position_attitude,
-    aspn23_lcm.measurement_position_velocity_attitude: aspn23_lcm.lcm_to_measurement_position_velocity_attitude,
-    aspn23_lcm.measurement_range_rate_to_point: aspn23_lcm.lcm_to_measurement_range_rate_to_point,
-    aspn23_lcm.measurement_range_to_point: aspn23_lcm.lcm_to_measurement_range_to_point,
-    aspn23_lcm.measurement_satnav: aspn23_lcm.lcm_to_measurement_satnav,
-    aspn23_lcm.measurement_satnav_subframe: aspn23_lcm.lcm_to_measurement_satnav_subframe,
-    aspn23_lcm.measurement_satnav_with_sv_data: aspn23_lcm.lcm_to_measurement_satnav_with_sv_data,
-    aspn23_lcm.measurement_specific_force_1d: aspn23_lcm.lcm_to_measurement_specific_force_1d,
-    aspn23_lcm.measurement_speed: aspn23_lcm.lcm_to_measurement_speed,
-    aspn23_lcm.measurement_TDOA_1Tx_2Rx: aspn23_lcm.lcm_to_measurement_TDOA_1Tx_2Rx,
-    aspn23_lcm.measurement_TDOA_2Tx_1Rx: aspn23_lcm.lcm_to_measurement_TDOA_2Tx_1Rx,
-    aspn23_lcm.measurement_temperature: aspn23_lcm.lcm_to_measurement_temperature,
-    aspn23_lcm.measurement_time: aspn23_lcm.lcm_to_measurement_time,
-    aspn23_lcm.measurement_time_difference: aspn23_lcm.lcm_to_measurement_time_difference,
-    aspn23_lcm.measurement_time_frequency_difference: aspn23_lcm.lcm_to_measurement_time_frequency_difference,
-    aspn23_lcm.measurement_velocity: aspn23_lcm.lcm_to_measurement_velocity,
     # aspn2_lcm to aspn23
     aspn2_lcm.accumulateddistancetraveled: aspn2_translations.translate_from_aspn2_accumulateddistancetraveled_to_aspn23,
     aspn2_lcm.altitude: aspn2_translations.translate_from_aspn2_altitude_to_aspn23,
@@ -296,15 +137,17 @@ marshaler_from_lcm: dict[
     aspn2_lcm.velocity3d: aspn2_translations.translate_from_aspn2_velocity3d_to_aspn23,
 }
 
+marshaler_from_lcm.update(marshaler_from_aspn23_lcm)  # type: ignore[arg-type]
+
 # dictionary mapping LCM message fingerprint to message decode function
-decoder: dict[bytes, Callable[[bytes], Aspn23LcmMeasurement | Aspn2LcmMeasurement]] = {}
+decoder: dict[bytes, Callable[[bytes], Aspn23LcmMsg | Aspn2LcmMsg]] = {}
 for aspn_type in marshaler_from_lcm:
     decoder[aspn_type._get_packed_fingerprint()] = aspn_type.decode  # type: ignore[assignment]
 
 
 def decode_aspn_lcm_msg(
     data: bytes,
-) -> Aspn23LcmMeasurement | Aspn2LcmMeasurement | None:
+) -> Aspn23LcmMsg | Aspn2LcmMsg | None:
     """
     Decodes a set of bytes into an ASPN-LCM message. Uses the first 8 bytes to determine the type of message,
     if the type cannot be determined this function will return ``None``.
@@ -313,7 +156,7 @@ def decode_aspn_lcm_msg(
         data (bytes): The set of bytes to decode.
 
     Returns:
-        Aspn23LcmMeasurement | None
+        Aspn23LcmMsg | None
     """
     fingerprint = data[:8]
 
@@ -325,17 +168,17 @@ def decode_aspn_lcm_msg(
 
 
 def marshal_from_lcm(
-    msg: Aspn23LcmMeasurement | Aspn2LcmMeasurement,
+    msg: Aspn23LcmMsg | Aspn2LcmMsg,
 ) -> Aspn23MeasurementExtended | None:
     """
     Converts from ASPN-LCM message to ASPN23 message. If the input message cannot be converted,
     this function will return ``None``.
 
     Args:
-        msg (``Aspn23LcmMeasurement``): The message to convert.
+        msg (``Aspn23LcmMsg``): The message to convert.
 
     Returns:
-        Aspn23Measurement | None
+        Aspn23Msg | None
     """
     msg_type = type(msg)
 
@@ -346,7 +189,7 @@ def marshal_from_lcm(
     return marshal_func(msg)
 
 
-def marshal_to_aspn23_lcm(msg: aspn23.AspnBase) -> Aspn23LcmMeasurement | None:
+def marshal_to_aspn23_lcm(msg: aspn23.AspnBase) -> Aspn23LcmMsg | None:
     """
     Convert from ASPN23 message to ASPN23-LCM message. If the input message cannot be converted,
     this function will return ``None``.
@@ -355,7 +198,7 @@ def marshal_to_aspn23_lcm(msg: aspn23.AspnBase) -> Aspn23LcmMeasurement | None:
         msg (AspnBase): The message to convert.
 
     Returns:
-        Aspn23LcmMeasurement | None
+        Aspn23LcmMsg | None
     """
     msg_type = type(msg)
 
@@ -363,10 +206,10 @@ def marshal_to_aspn23_lcm(msg: aspn23.AspnBase) -> Aspn23LcmMeasurement | None:
         return None
 
     marshal_func = marshaler_to_aspn23_lcm[msg_type]  # type: ignore[index]
-    return marshal_func(msg)
+    return marshal_func(msg)  # type: ignore[no-any-return]
 
 
-def marshal_to_aspn2_lcm(msg: aspn23.AspnBase) -> Aspn2LcmMeasurement | None:
+def marshal_to_aspn2_lcm(msg: aspn23.AspnBase) -> Aspn2LcmMsg | None:
     """
     Convert from ASPN23 message to ASPN2-LCM message. If the input message cannot be converted,
     this function will return ``None``.
@@ -375,7 +218,7 @@ def marshal_to_aspn2_lcm(msg: aspn23.AspnBase) -> Aspn2LcmMeasurement | None:
         msg (AspnBase): The message to convert.
 
     Returns:
-        Aspn2LcmMeasurement | None
+        Aspn2LcmMsg | None
     """
     msg_type = type(msg)
 
@@ -449,8 +292,8 @@ def process_lcm_message(
 def create_lcm_message(
     message: Message,
     output_version: AspnVersion,
-) -> Aspn2LcmMeasurement | Aspn23LcmMeasurement | None:
-    lcm_msg: Aspn2LcmMeasurement | Aspn23LcmMeasurement | None = None
+) -> Aspn2LcmMsg | Aspn23LcmMsg | None:
+    lcm_msg: Aspn2LcmMsg | Aspn23LcmMsg | None = None
     if output_version == AspnVersion.V2:
         lcm_msg = marshal_to_aspn2_lcm(message.wrapped_message)
     else:
