5938384f34ef0541163d94e321a1889db3f52148
Henry Raquet <henry.raquet@is4s.com>
2026-04-13 18:15:00 +0000

    Feature/utility plugin doc clarification

--- pntos-api/src/pntos/api/plugins/utility.py
+++ pntos-api/src/pntos/api/plugins/utility.py
@@ -9,9 +9,9 @@ class UtilityPlugin(CommonPlugin, ABC):
     """
     A plugin that performs a generic utility function.
 
-    A utility plugin performs functions that may require access to pntOS resources (such
-    as the registry) but is not otherwise relied upon to perform any particular
-    function.
+    A utility plugin performs implementation-specific functions that may require access
+    to pntOS resources (such as the registry) via the :class:`pntos.api.CommonPlugin`
+    API. Otherwise, this plugin has no other API-defined functionality.
     """
 
     pass
