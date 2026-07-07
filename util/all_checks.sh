d48d8a3344c71dae270ea3720c30bd67c3bae4b2
Daniel Jacobs <daniel.jacobs@is4s.com>
2026-07-07 15:26:49 -0400

    set all coverage flags except --cov in pyproject.toml
    These flags will only be used when coverage is enabled by adding --cov when invoking pytest.

--- util/all_checks.sh
+++ util/all_checks.sh
@@ -18,7 +18,7 @@ ty check
 util/check_sync.sh
 ret_val=$?  # this must be set after check_sync to observe if it passed or not
 util/build_docs.sh
-pytest pntos-cobra pntos-extras --cov --cov-fail-under=75 --cov-report={term,html} --cov-config=.coveragerc
+pytest pntos-cobra pntos-extras --cov
 pytest apps -s
 python3 util/api_synchronization/compare_apis.py
 
--- util/quick_checks.sh
+++ util/quick_checks.sh
@@ -18,7 +18,7 @@ ty check
 util/check_sync.sh
 ret_val=$?  # this must be set after check_sync to observe if it passed or not
 util/build_docs.sh
-pytest pntos-cobra pntos-extras --cov --cov-fail-under=75 --cov-report={term,html} --cov-config=.coveragerc
+pytest pntos-cobra pntos-extras --cov
 
 # don't print the following commands
 set +x
